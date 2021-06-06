845f0eaf1e5f8eabee9a3f7107c23d11d12f0461
HuskyyTrzaski <kubajochymek@gmail.com>
2021-06-06 18:16:31 +0200

    zaczynam edycje

--- zadanie7.cpp
+++ zadanie7.cpp
@@ -1,31 +1,4 @@
-//S
-//Proszê stworzyæ w jednym pliku klasê Macierz2D, która bêdzie mog³a przechowywaæ macierze kwadratowe o rozmiarach 3 na 3 z elementami typu float(float** m_dane).Ponadto w klasie nale¿y :
-//Stworzyæ konstruktory :
-//-Bez parametrów.
-//- Z jednym parametrem typu float.
-//- Przyjmuj¹cy jako argument tablicê dwuwymiarow¹ elementów typu float.
-//- Przyjmuj¹cy jako argument listê inicjuj¹c¹.
-//Przeci¹¿yæ operatory :
-//-przypisania =
-//-dodawania +
-//-dodawania z przypisaniem +=
-//-odejmowania -
-//-odejmowania z przypisaniem -=
-//-Post i pre zarówno inkrementacji jak i dekrementacji++ --
-//- mno¿enia * (przez liczbê ca³kowit¹)
-//- mno¿enia z przypisaniem *= (przez liczbê ca³kowit¹)
-//- mno¿enia * (macierzy)
-//-mno¿enia z przypisaniem *= (macierzy)
-//-operatory strumieniowego wejœcia i wyjœcia >> << (wyœwietlanie powinno byæ sformatowane)
-//Dodaæ metody realizuj¹ce :
-//-Ustawianie i zwracanie konkretnych elementów macierzy.
-//- Stworzenie macierzy jednostkowej.
-//- Transponowanie macierzy.
-//
-//W przypadku tych operatorów, które mog¹ byæ przeci¹¿one zarówno poza jak i w klasie mog¹ Pañstwo wybraæ dowolnie.
-//Proszê równie¿ zaimplementowaæ w main tablicê przechowuj¹c¹ trzy ró¿ne macierze typu Macierz2D.Proszê posortowaæ tê tablicê za pomoc¹ sortowania z biblioteki standardowej.
-//W main proszê umieœciæ przyk³adowe mno¿enie macierzy ³¹cznie z wypisaniem operandów i wyniku.
-//Nale¿y zadbaæ o poprawn¹ obs³ugê obiektów klasy Macierz2D.
+//https://github.com/HuskyyTrzadki/zadanie7
 #include <iostream>
 #include <initializer_list>
 //dzien Dobry, ¿yczê mi³ego Dnia.
